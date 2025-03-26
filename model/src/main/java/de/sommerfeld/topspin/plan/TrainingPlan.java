c1393aad979bfc5181913c0e641d6248f147e0f1
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-26 01:28:33 +0100

    feat: add units to training plan

--- model/src/main/java/de/sommerfeld/topspin/plan/TrainingPlan.java
+++ model/src/main/java/de/sommerfeld/topspin/plan/TrainingPlan.java
@@ -1,12 +1,16 @@
 package de.sommerfeld.topspin.plan;
 
+import de.sommerfeld.topspin.plan.components.TrainingUnits;
+
 public class TrainingPlan {
 
     private final String name;
     private final String description;
+    private final TrainingUnits trainingUnits;
 
-    public TrainingPlan(String name, String description) {
+    public TrainingPlan(String name, String description, TrainingUnits trainingUnits) {
         this.name = name;
         this.description = description;
+        this.trainingUnits = trainingUnits;
     }
 }
