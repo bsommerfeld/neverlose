94623f85f91e51b8b4aa440a6357694beeaefa83
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-27 01:12:41 +0100

    refactor: spacing

--- client/src/main/java/de/sommerfeld/topspin/Main.java
+++ client/src/main/java/de/sommerfeld/topspin/Main.java
@@ -4,6 +4,7 @@ import de.sommerfeld.topspin.bootstrap.Bootstrap;
 import de.sommerfeld.topspin.bootstrap.TopspinBootstrap;
 
 public class Main {
+
     public static void main(String[] args) {
         Bootstrap topspinBootstrap = new TopspinBootstrap();
         topspinBootstrap.start();
