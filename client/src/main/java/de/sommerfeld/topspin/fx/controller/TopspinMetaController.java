f22b85e4b9eb3d76f74295f12fb41c2979f97d1d
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-27 08:08:39 +0100

    feat: add center content handling in TopspinMetaController
    Introduced a new AnchorPane `centerContentPlaceholder` in the layout and updated the controller to load and anchor center content dynamically. This includes a method to set anchors for flexible content placement and integration of a view for the center section.

--- client/src/main/java/de/sommerfeld/topspin/fx/controller/TopspinMetaController.java
+++ client/src/main/java/de/sommerfeld/topspin/fx/controller/TopspinMetaController.java
@@ -4,6 +4,7 @@ import de.sommerfeld.topspin.fx.view.View;
 import de.sommerfeld.topspin.fx.view.ViewProvider;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 /**
@@ -18,21 +19,38 @@ public class TopspinMetaController {
     private HBox topBarPlaceholder;
 
     @FXML
+    private AnchorPane centerContentPlaceholder;
+
+    @FXML
     private HBox bottomBarPlaceholder;
 
     public TopspinMetaController() {
         this.viewProvider = new ViewProvider(); // TODO: Injection
     }
 
+    private static void setAnchor(Parent center) {
+        AnchorPane.setTopAnchor(center, 0d);
+        AnchorPane.setLeftAnchor(center, 0d);
+        AnchorPane.setRightAnchor(center, 0d);
+        AnchorPane.setBottomAnchor(center, 0d);
+    }
+
     @FXML
     private void initialize() {
         loadTopBar();
+        loadCenter();
         loadBottomBar();
     }
 
     private void loadTopBar() {
         Parent topBar = viewProvider.requestView(TopBarController.class).parent();
         topBarPlaceholder.getChildren().add(topBar);
+    }
+
+    private void loadCenter() {
+        Parent center = viewProvider.requestView(TrainingPlanEditorController.class).parent();
+        setAnchor(center);
+        centerContentPlaceholder.getChildren().add(center);
     }
 
     private void loadBottomBar() {
