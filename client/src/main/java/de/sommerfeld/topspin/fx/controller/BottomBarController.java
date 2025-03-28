32cec835abdcde42b62fd7de81302b6a98186dfb
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-28 11:55:13 +0100

    feat: implement new plan logic

--- client/src/main/java/de/sommerfeld/topspin/fx/controller/BottomBarController.java
+++ client/src/main/java/de/sommerfeld/topspin/fx/controller/BottomBarController.java
@@ -1,7 +1,28 @@
 package de.sommerfeld.topspin.fx.controller;
 
+import com.google.inject.Inject;
 import de.sommerfeld.topspin.fx.view.View;
+import de.sommerfeld.topspin.fx.view.ViewProvider;
+import de.sommerfeld.topspin.plan.TrainingPlan;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 @View
 public class BottomBarController {
+
+    private final ViewProvider viewProvider;
+
+    @Inject
+    public BottomBarController(ViewProvider viewProvider) {
+        this.viewProvider = viewProvider;
+    }
+
+    @FXML
+    public void onNewPlan(ActionEvent actionEvent) {
+        viewProvider.triggerViewChange(TrainingPlanEditorController.class, p -> p.setPlan(new TrainingPlan("New Plan", "")));
+    }
+
+    @FXML
+    public void onShowAllPlans(ActionEvent actionEvent) {
+    }
 }
