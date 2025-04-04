ae98bbd2f818d111950061f23bd27b8c9900bf32
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-04-04 11:16:32 +0200

    chore: bind ExportService to PdfExportService for future injection design

--- client/src/main/java/de/sommerfeld/topspin/bootstrap/TopspinModule.java
+++ client/src/main/java/de/sommerfeld/topspin/bootstrap/TopspinModule.java
@@ -2,6 +2,8 @@ package de.sommerfeld.topspin.bootstrap;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import de.sommerfeld.topspin.export.ExportService;
+import de.sommerfeld.topspin.export.PdfExportService;
 import de.sommerfeld.topspin.fx.view.ViewLoader;
 import de.sommerfeld.topspin.fx.view.ViewProvider;
 
@@ -10,6 +12,7 @@ public class TopspinModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Bootstrap.class).to(TopspinBootstrap.class);
+        bind(ExportService.class).to(PdfExportService.class);
 
         bind(ViewProvider.class).asEagerSingleton();
         bind(ViewLoader.class).in(Singleton.class);
