ea0e222ee3f223707290533ea00a99aa4669566e
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-04-22 01:02:28 +0200

    refactor: removed ViewProvider from BottomBar since its not needed anymore

--- client/src/main/java/de/sommerfeld/topspin/fx/controller/BottomBarController.java
+++ client/src/main/java/de/sommerfeld/topspin/fx/controller/BottomBarController.java
@@ -1,16 +1,7 @@
 package de.sommerfeld.topspin.fx.controller;
 
-import com.google.inject.Inject;
 import de.sommerfeld.topspin.fx.view.View;
-import de.sommerfeld.topspin.fx.view.ViewProvider;
 
 @View
 public class BottomBarController {
-
-    private final ViewProvider viewProvider;
-
-    @Inject
-    public BottomBarController(ViewProvider viewProvider) {
-        this.viewProvider = viewProvider;
-    }
 }
